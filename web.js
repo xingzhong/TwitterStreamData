bcd44143890d56d5cc493a1337abf47e87e19b6d
Xingzhong <xu.xeno@gmail.com>
2013-01-20 13:39:38 -0500

    add server selection

--- web.js
+++ web.js
@@ -23,10 +23,17 @@ app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
+var twit;
 io.sockets.on("connection", function(socket){
     io.sockets.emit("status", {"status" : "socket ready!"});
     socket.on("test", function(data) {
         io.sockets.emit("res", data);
+    });
+    socket.on("enterUser", function(user) {
+        if (user == "ws1") {twit = twit1;}
+        if (user == "ws2") {twit = twit2;}
+        if (user == "ws3") {twit = twit3;}
+        io.sockets.emit("userRec", user);
     });
     socket.on("keyword", function(key) {
         console.log(key);
@@ -38,17 +45,24 @@ io.sockets.on("connection", function(socket){
     });
 });
 
-/*
-var twit = new twitter({
+
+var twit1 = new twitter({
     consumer_key: 'verwURhvFVTuojYVJykZQ',
 	consumer_secret: 'RQDgjMrlen8QdZJM90rK9zxvCtSlJXfU7I7YO2STELk',
 	access_token_key: '26635865-5GyKYJD8oqteN7VvklzWE7Vtsssovorvo90P8izoc',
 	access_token_secret: 'kLZebpapNT9bQpCYXSoaJJojIpebukyNrjb8HSmVwA'
-});*/
+});
 
-var twit = new twitter({
+var twit2 = new twitter({
     consumer_key: 'o7hQyqC0Ba8csgohX4sPsQ',
     consumer_secret: 'HSyl9wxdaZwHpvolAWEDxdFDMrmKxla2hStLGNbSllM',
 	access_token_key: '26635865-ug3bmGiHmLGxEhSBIQkRPOEzJ7ZiEX5oP298qPF6U',
 	access_token_secret: '1bPjIMR9EulJB7Uvb8XkNH6tTnEVKGvGYDv4pnt1Ds'
+});
+
+var twit3 = new twitter({
+    consumer_key: 'JEfKLDMpaMxZEQEynEdNQ',
+    consumer_secret: '2iNJRZYb5HYrd9V8TPHtMPIGUQizQlHVaTlsgPjGM',
+    access_token_key: '26635865-PWr5miIeOauR7kZecWug01uPRjfbyyC4rMIGqUkvc',
+	access_token_secret: 'EkuBb4zUOYh8NIIWEoRjVeYPEFtfogmeFzESHxZc5A'
 });
